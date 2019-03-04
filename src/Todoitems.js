d84e03f9228ded10853c31ad37cc9409d330b78c
Ilya <ilya@movableink.com>
2019-03-04 16:30:26 -0500

    added delete method

---
+++ src/Todoitems.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import FlipMove from "react-flip-move";
+
+class TodoItems extends Component {
+  constructor(props) {
+    super(props);
+
+    this.createTask = this.createTask.bind(this);
+  }
+
+  createTask(items) {
+    return (
+      <li
+        onClick={() => this.delete(items.key)}
+        className="list-group-item"
+        key={items.key}
+      >
+        {items.text}
+      </li>
+    );
+  }
+
+  delete(key) {
+    this.props.delete(key);
+  }
+
+  render() {
+    let todoEntries = this.props.entries;
+    let listItems = todoEntries.map(this.createTask);
+
+    return (
+      <ul className="theList list-group list-group-flush">
+        <FlipMove duration={200} easing="ease-in">
+          {listItems}
+        </FlipMove>
+      </ul>
+    );
+  }
+}
+
+export default TodoItems;
---
+++ src/toDoList.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import ToDoItems from "./Todoitems";
+
+class toDoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addItem(e) {
+    if (this._inputElement.value !== "") {
+      const newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+
+      this.setState(prevState => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      });
+    }
+
+    this._inputElement.value = "";
+
+    console.log(this.state.items);
+
+    e.preventDefault();
+  }
+
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function(item) {
+      return item.key !== key;
+    });
+
+    this.setState({
+      items: filteredItems
+    });
+  }
+
+  render() {
+    return (
+      <div className="toDoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input
+              ref={a => (this._inputElement = a)}
+              placeholder="enter task"
+              autoFocus
+            />
+            <button className="btn btn-sm btn-primary" type="submit">
+              add
+            </button>
+          </form>
+        </div>
+        <ToDoItems entries={this.state.items} delete={this.deleteItem} />
+      </div>
+    );
+  }
+}
+
+export default toDoList;
